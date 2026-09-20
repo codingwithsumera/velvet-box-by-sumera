fbdfefb081a676f4f798b09f6fca5e271bdbefd6
codingwithsumera <sumerashafi1999@gmail.com>
2026-09-20 22:40:04 +0500

    End of day updates

--- velvet-box-by-sumera/src/app.module.ts
+++ velvet-box-by-sumera/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { CategoriesModule } from './categories/categories.module.js';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
@@ -14,6 +15,7 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
       appSecret: 'YOUR_APP_SECRET',
       serviceId: 'velvet-box-by-sumera',
     }),
+    CategoriesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
